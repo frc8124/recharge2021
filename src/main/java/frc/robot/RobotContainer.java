9214d53751a7f61f7c76036c3427871509047465
frc8124 <frc8124@schartz.net>
2021-02-09 17:07:54 -0500

    New crew onboard

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -64,7 +64,7 @@ public class RobotContainer {
    * Use this method to define your button->command mappings.  Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}. QLM was here
    */
   private void configureButtonBindings() {
   
@@ -121,6 +121,7 @@ public class RobotContainer {
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
+   * as was here
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
