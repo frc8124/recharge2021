24f4c77ff05fda5d631e21916f33c3dfcf41d52c
frc8124 <frc8124@schartz.net>
2021-02-09 17:02:33 -0500

    Test commit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,7 +19,7 @@ import edu.wpi.first.wpilibj.util.Color;
  *
  * <p>
  * It is advised to statically import this class (or one of its inner classes)
- * wherever the constants are needed, to reduce verbosity.
+ * wherever the constants are needed, to reduce verbosity..
  */
 public final class Constants {
 
